ef400eb25bc4681cbdf726606d5cb1a2ec68f473
Reenta Kim <william@jicode.co.kr>
2024-06-27 16:46:51 +0900

    read csv file

--- src/main/java/com/example/calendarapp/CalendarAppApplication.java
+++ src/main/java/com/example/calendarapp/CalendarAppApplication.java
@@ -3,39 +3,21 @@ package com.example.calendarapp;
 
 import com.example.calendarapp.event.Meeting;
 import com.example.calendarapp.event.Schedule;
-import com.example.calendarapp.event.Todo;
+import com.example.calendarapp.reader.EventCsvReader;
 
-import java.time.ZonedDateTime;
-import java.util.HashSet;
+import java.io.IOException;
+import java.util.List;
 
 public class CalendarAppApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Schedule schedule = new Schedule();
-        HashSet<String> participants = new HashSet<>();
-        participants.add("reenta.kim");
 
-        Meeting meeting1 = new Meeting(
-                1, "meeting1", ZonedDateTime.now(), ZonedDateTime.now().plusHours(1),
-                participants, "meetingRoomA", "스터디"
-        );
+        EventCsvReader csvReader = new EventCsvReader();
+        String meetingCsvPath = "/data/meeting.csv";
 
-        schedule.add(meeting1);
-
-        Todo todo1 = new Todo(
-                2, "todo1", ZonedDateTime.now().plusHours(3), ZonedDateTime.now().plusHours(4),
-                "할 일 적기"
-        );
-        schedule.add(todo1);
-
-        Todo todo2 = new Todo(
-                3, "todo2", ZonedDateTime.now().plusHours(5), ZonedDateTime.now().plusHours(4),
-                "할 일 적기222"
-        );
-        schedule.add(todo2);
-//        list.forEach(Event::print);
-
-//        schedule.printBy(EventType.TO_DO);
+        List<Meeting> meetings = csvReader.readMeetings(meetingCsvPath);
+        meetings.forEach(schedule::add);
 
         schedule.printAll();
     }
